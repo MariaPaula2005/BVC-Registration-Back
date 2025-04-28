844539c49f88b1c2e58eae26fdb8006868518b95
MariaPaula2005 <mapamora05@gmail.com>
2025-04-28 01:22:00 -0600

    Backend update

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ const jwt = require('jsonwebtoken');
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000', 
-    credentials: true, 
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
+    credentials: true,
   };
+  
 
   app.use(cors(corsOptions));
 
@@ -30,16 +31,17 @@ app.use(session({
     secret: 'your-secret-key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } 
-  }));
+    cookie: { secure: true }
+}));
 
 //connection to mongodb
 const dbURI = 'mongodb+srv://Maria:12345@bvcregistration.qf6qh.mongodb.net/?retryWrites=true&w=majority&appName=BVCRegistration'; 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
-    app.listen(5000, () => {
-        console.log('Server running on port 5000');
-    });
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 })
 .catch(err => console.log(err));
 
